71bf3c6bd1ea8a882c341db14445269a5b26a905
soraia-aparecida <soraia211norte@gmail.com>
2021-11-23 14:05:47 -0300

    Pagina quero ser um ninja

--- src/components/SerUmNinja.js
+++ src/components/SerUmNinja.js
@@ -4,7 +4,6 @@ import styled from "styled-components";
 const Cadastro = styled.div`
     display: flex;
     flex-direction: column;
-    -webkit-box-align: center;
     align-items: center;
 
     input{
@@ -21,41 +20,41 @@ const Cadastro = styled.div`
     }
 `
 
-class SerUmNinja extends React.Component{
-    render(){
-        return(
+class SerUmNinja extends React.Component {
+    render() {
+        return (
             <Cadastro>
-          <h1>Cadastre o seu serviço</h1>
-          <input
-          placeholder="Título"
-          // value={}
-          // onChange={}
-          />
-          <input 
-           placeholder="Descrição"
-          //  value={}
-          //  onChange={}
-           />
-          <input
-           placeholder="Preço"
-          //  value={}
-          //  onChange={}
-           type="number"
-           />
-           <select multiple>
-             <option>Boleto</option>
-             <option>Cartão de crédito</option>
-             <option>Cartão de débito</option>
-             <option>Pix</option>
-             <option>Playpal</option>
-           </select>
-           <input
-           placeholder="Prazo de serviço"
-          //  value={}
-          //  onChange={}
-           type="date"/>
-           <button >Cadastrar Serviço</button>
-        
+                <h1>Cadastre o seu serviço</h1>
+                <input
+                    placeholder="Título"
+                // value={}
+                // onChange={}
+                />
+                <input
+                    placeholder="Descrição"
+                //  value={}
+                //  onChange={}
+                />
+                <input
+                    placeholder="Preço"
+                    //  value={}
+                    //  onChange={}
+                    type="number"
+                />
+                <select multiple>
+                    <option>Boleto</option>
+                    <option>Cartão de crédito</option>
+                    <option>Cartão de débito</option>
+                    <option>Pix</option>
+                    <option>Playpal</option>
+                </select>
+                <input
+                    placeholder="Prazo de serviço"
+                    //  value={}
+                    //  onChange={}
+                    type="date" />
+                <button >Cadastrar Serviço</button>
+
             </Cadastro>
         )
     }
