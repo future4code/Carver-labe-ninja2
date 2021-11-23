6038629d5252bc81abc666630f5289a2b2dea36e
Madreyv <madreyv@gmail.com>
2021-11-23 14:23:46 -0300

    testado correção de conflito

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
-import SecaoContratar from './SecaoContratar/SecaoContratar'
+
 
 
 export class AppContainer extends Component {
   render() {
     return (
       <div>
-        <SecaoContratar/>
       </div>
     )
   }
