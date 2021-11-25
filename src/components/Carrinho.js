12308cf9d3e05deeecb341e1d02f0c47ca2fdf41
Anna Isabella Gomes Quaranta <bellaquaranta@gmail.com>
2021-11-25 09:13:51 -0300

    Funcionalidades do carrinho adicionadas

--- src/components/Carrinho.js
+++ src/components/Carrinho.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import styled from 'styled-components'
 
 
-const ItemCarrinho=styled.div` 
+const ItemCarrinho = styled.div` 
 border: 1px solid black;
 display: flex;
 justify-content: space-between;
@@ -14,7 +14,7 @@ button{
   cursor: pointer;
 }
 `
-const CarrinhoTotal=styled.div`
+const CarrinhoTotal = styled.div`
 display: flex;
 align-items: center;
 padding: 1%;
@@ -28,52 +28,48 @@ button{
 `
 
 export default class Carrinho extends Component {
-  state={
-    carrinho:[
-      {
-        nome:"tst",
-        preço:200,
-      },
-        {
-        nome:"tst",
-        preço:200,
-      },
-        {
-        nome:"tst",
-        preço:200,
-      },
-        {
-        nome:"tst",
-        preço:900,
-      },
-  ], 
-  total:0
+
+  finalizarCompra = () => {
+    alert("obrigada por comprar com a gente!!!!!!!")
+    this.props.invocarTela("contratarUmNinja")
+    this.props.atualizarCarrinho([])
   }
+
+  removerItem = (id) => {
+    const carrinhoSemItem = this.props.statusCarrinho.produtos.filter((item) => {
+      return item.id !== id
+    })
+    this.props.atualizarCarrinho(carrinhoSemItem)
+  }
+
   render() {
-    let soma=0
-    let valorTotal = this.state.carrinho.map((valor) => {
-      soma = valor.preço + soma
+
+    let soma = 0
+    let valorTotal = this.props.carrinho.produtos.map((valor) => {
+      soma = valor.price + soma
       return soma
     })
-    let valorResultante=valorTotal.pop()
-    console.log(valorTotal,valorResultante)
-    let itens=this.state.carrinho.map((item)=>{
-      return  <ItemCarrinho>
-        <p>{item.nome}</p>
-        <p>{`R$${item.preço} `}</p>
-        <button>Remover</button>
+
+    let valorResultante = valorTotal.pop()
+
+    let itens = this.props.carrinho.produtos.map((item) => {
+      return <ItemCarrinho> <p>{item.title}</p>
+        <p>{`R$${item.price} `}</p>
+        <button onClick={() => this.removerItem(item.id)}>Remover</button>
       </ItemCarrinho>
     })
-    let resultado=<CarrinhoTotal>
+
+    let resultado = <CarrinhoTotal>
       <p>{`Total: R$${valorResultante}`}</p>
-      <button>Finalizar Compra</button>
-      <button onClick={() => this.props.invocarTela("contratarUmNinja")}mm>Voltar Para Lista</button>
-      </CarrinhoTotal>
+      <button onClick={this.finalizarCompra}>Finalizar Compra</button>
+
+      <button onClick={() => this.props.invocarTela("contratarUmNinja")}>Voltar Para Lista</button>
+    </CarrinhoTotal>
+
     return (
       <div>
-      {this.state.carrinho.length>0? itens  : <h1>Carrinho</h1> }
-      {this.state.carrinho.length>0? resultado : ' ' }
-
+        {this.props.carrinho.produtos.length > 0 ? itens : <h1>Carrinho</h1>}
+        {this.props.carrinho.produtos.length > 0 ? resultado : ' '}
       </div>
     )
   }
