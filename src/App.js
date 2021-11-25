0d4662e51a4334a3ff82f539a208a7dd67f057c0
Madreyv <madreyv@gmail.com>
2021-11-25 12:07:06 -0300

    Adcionado integração de carregar componentes, carregamento automatico dos cards e função de adcionar ao carrinho

--- src/App.js
+++ src/App.js
@@ -25,114 +25,129 @@ class App extends React.Component {
 	state = {
 		tela: "home",
 		anuncios: [
-			{
-				id: "efed9385-cf87-4f0e-a121-465384b3f2e4",
-				title: "Cortar a grama",
-				description: "Manutenção em áreas verdes de até 1000 metros quadrados.",
-				price: 40,
-				paymentMethods: [
-					"PayPal",
-					"boleto"
-				],
-				dueDate: "2021-12-30",
-				taken: false
-			},
-			{
-				id: "a",
-				title: "Oiii",
-				description: "ççççç.",
-				price: 4000,
-				paymentMethods: [
-					"PayPal",
-					"boleto"
-				],
-				dueDate: "2221-12-30",
-				taken: false
-			},
-			{
-				id: "e",
-				title: "hoje",
-				description: "uuuuu.",
-				price: 5,
-				paymentMethods: [
-					"PayPal",
-					"boleto"
-				],
-				dueDate: "2050-12-30",
-				taken: false
-			},
-			{
-				id: "kk",
-				title: "Testettsvsg",
-				description: "jjjj.",
-				price: 500,
-				paymentMethods: [
-					"PayPal",
-					"boleto"
-				],
-				dueDate: "2090-12-30",
-				taken: false
-			}
+			// {
+			// 	id: "efed9385-cf87-4f0e-a121-465384b3f2e4",
+			// 	title: "Cortar a grama",
+			// 	description: "Manutenção em áreas verdes de até 1000 metros quadrados.",
+			// 	price: 40,
+			// 	paymentMethods: [
+			// 		"PayPal",
+			// 		"boleto"
+			// 	],
+			// 	dueDate: "2021-12-30",
+			// 	taken: false
+			// },
+			// {
+			// 	id: "a",
+			// 	title: "Oiii",
+			// 	description: "ççççç.",
+			// 	price: 4000,
+			// 	paymentMethods: [
+			// 		"PayPal",
+			// 		"boleto"
+			// 	],
+			// 	dueDate: "2221-12-30",
+			// 	taken: false
+			// },
+			// {
+			// 	id: "e",
+			// 	title: "hoje",
+			// 	description: "uuuuu.",
+			// 	price: 5,
+			// 	paymentMethods: [
+			// 		"PayPal",
+			// 		"boleto"
+			// 	],
+			// 	dueDate: "2050-12-30",
+			// 	taken: false
+			// },
+			// {
+			// 	id: "kk",
+			// 	title: "Testettsvsg",
+			// 	description: "jjjj.",
+			// 	price: 500,
+			// 	paymentMethods: [
+			// 		"PayPal",
+			// 		"boleto"
+			// 	],
+			// 	dueDate: "2090-12-30",
+			// 	taken: false
+			// }
 	
 		],
 		carrinho: {
 			valoTotal: 0,
 			produtos: [
-				{
-					"id": "efed9385-cf87-4f0e-a121-465384b3f2e4",
-					"title": "Cortar a grama",
-					"description": "Manutenção em áreas verdes de até 1000 metros quadrados.",
-					"price": 40,
-					"paymentMethods": [
-						"PayPal",
-						"boleto"
-					],
-					"dueDate": "2021-12-30T00:00:00.000Z",
-					"taken": false
-				},
-				{
-					"id": "efed9385-cf87-4f0e-a121-465384b3f2e6",
-					"title": "brincar com lama",
-					"description": "Manutenção em áreas verdes de até 1000 metros quadrados.",
-					"price": 60,
-					"paymentMethods": [
-						"PayPal",
-						"boleto"
-					],
-					"dueDate": "2021-12-30T00:00:00.000Z",
-					"taken": false
-				},
-				{
-					"id": "efed9385-cf87-4f0e-a121-465384b3f2e5",
-					"title": "Fazer unha",
-					"description": "Manutenção em áreas verdes de até 1000 metros quadrados.",
-					"price": 50,
-					"paymentMethods": [
-						"PayPal",
-						"boleto"
-					],
-					"dueDate": "2021-12-30T00:00:00.000Z",
-					"taken": false
-				}]
+				// {
+				// 	"id": "efed9385-cf87-4f0e-a121-465384b3f2e4",
+				// 	"title": "Cortar a grama",
+				// 	"description": "Manutenção em áreas verdes de até 1000 metros quadrados.",
+				// 	"price": 40,
+				// 	"paymentMethods": [
+				// 		"PayPal",
+				// 		"boleto"
+				// 	],
+				// 	"dueDate": "2021-12-30T00:00:00.000Z",
+				// 	"taken": false
+				// },
+				// {
+				// 	"id": "efed9385-cf87-4f0e-a121-465384b3f2e6",
+				// 	"title": "brincar com lama",
+				// 	"description": "Manutenção em áreas verdes de até 1000 metros quadrados.",
+				// 	"price": 60,
+				// 	"paymentMethods": [
+				// 		"PayPal",
+				// 		"boleto"
+				// 	],
+				// 	"dueDate": "2021-12-30T00:00:00.000Z",
+				// 	"taken": false
+				// },
+				// {
+				// 	"id": "efed9385-cf87-4f0e-a121-465384b3f2e5",
+				// 	"title": "Fazer unha",
+				// 	"description": "Manutenção em áreas verdes de até 1000 metros quadrados.",
+				// 	"price": 50,
+				// 	"paymentMethods": [
+				// 		"PayPal",
+				// 		"boleto"
+				// 	],
+				// 	"dueDate": "2021-12-30T00:00:00.000Z",
+				// 	"taken": false
+				// }
+			]
 		}
 	}
 
-	addCarrinho = (servico) => {
-		let servicoSelecionado = [...this.state.carrinho, servico]
-		this.setState({carrinho: servicoSelecionado})
-		alert(`O serviço foi adicionado ao seu carrinho`)
-	  }
-
 	componentDidMount = () => {
-		//this.carregarJobs()
+		console.log('aqui')
+		this.carregarJobs()
 		//this.atualizarJobs()
 		//this.pegarJob()
 		//this.deletarJobs()
 		//this.criarJobs()
 	}
 
+	addCarrinho = (servico) => {
+		let novoServico = {
+			valorTotal : 0,
+			produtos:[...this.state.carrinho.produtos, servico]
+		}
+		// let servicoSelecionado = [...this.state.carrinho, novoServico]
+
+		console.log(novoServico)
+		this.setState({carrinho: novoServico})
+		//alert(`O serviço foi adicionado ao seu carrinho`)
+	}
+
 	carregarJobs = () => {
-		getServicos()
+		let servicos = getServicos()
+		servicos.then((res) => {
+			this.setState({
+				anuncios: res
+			})
+			return servicos
+
+		})
 	}
 
 	pegarJob = (id) => {
@@ -196,6 +211,7 @@ class App extends React.Component {
 	//RENDER-----------------------------------------------------------------------------------------------------------
 
 	render() {
+		//console.log(this.state.anuncios)
 		return (
 			<div>
 				<GlobalStyle />
